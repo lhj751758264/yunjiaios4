0e737f77f38c71d50ff1dc16e95cff0b551dd4c0
Mac <mac@macdemacbook-pro.local>
2016-12-02 11:21:48 +0800

    12月2日创建的项目

---
+++ ios4/ios4/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ios4
+//
+//  Created by Mac on 16/12/2.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // lihaojie 创建的项目
+}
+
+@end
